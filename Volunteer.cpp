19616b608585397c7ca02150d1428e555910963a
pheintz <pat.iscool@gmail.com>
2014-10-13 22:06:26 -0700

    Create Volunteer.cpp

---
+++ Volunteer.cpp
@@ -0,0 +1,39 @@
+#include "Volunteer.h"
+
+
+Volunteer::Volunteer(void)
+{
+}
+
+void setFirst(int f)
+{
+	first = f;
+}
+void setSecond(int s)
+{
+	second = s;
+}
+void setThird(int t)
+{
+	third = t;
+}
+void getName(string n)
+{
+	name = n;
+}
+int getFirst();
+{
+	return first;
+}
+int getSecond();
+{
+	return second;
+}
+int getThird();
+{
+	return third;
+}
+string getName();
+{
+	return name
+}
