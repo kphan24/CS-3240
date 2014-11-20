e483b99301c2c8263b6329896cd5fed720ad5474
pheintz <pat.iscool@gmail.com>
2014-11-19 17:58:14 -0800

    Create HW7_Main.cpp

---
+++ HW7_Main.cpp
@@ -0,0 +1,15 @@
+#include "MatrixStructure.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void menu(MatrixStructure matrix);
+void readFromFile(string fileName);
+
+
+int main()
+{
+    MatrixStructure *matrix;
+    matrix = new MatrixStructure;
+}
