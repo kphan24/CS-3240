de372b379190f6ddc8668a91b8ae4d402c5ffa60
pheintz <pat.iscool@gmail.com>
2014-11-19 18:32:28 -0800

    Update HW7_Main.cpp

--- HW7_Main.cpp
+++ HW7_Main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 void menu(MatrixStructure matrix);
 void readFromFile(string fileName);
-
+//need read in and read out functions, these functions pass to functions
+//in the class cpp file which should correctly place the values in the list
 
 int main()
 {
