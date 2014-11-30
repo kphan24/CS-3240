db7fb3e171635dbfc96f7b6d7972923cab9d68e2
pheintz <pat.iscool@gmail.com>
2014-11-30 13:16:52 -0800

    Update HW8_Bintree.h

--- HW8_Bintree.h
+++ HW8_Bintree.h
@@ -1,5 +1,12 @@
-#ifndef HEINTZ_BINTREE_H_INCLUDED
-#define HEINTZ_BINTREE_H_INCLUDED
+/*
+    Patrick Heintz, Khoa Phan
+    HW8 Keywords From Text
+    CODE BLOCKS
+    WIN 7 64 bit
+*/
+
+#ifndef HW8_BinTree_GR9_H_INCLUDED
+#define HW8_BinTree_GR9_H_INCLUDED
 #include <stdio.h>
 
 typedef struct lineList LIST;
@@ -7,12 +14,12 @@ typedef struct nodeTag NODE;
 
 void treeToFile(NODE *root, FILE *fp);
 void destroyList(LIST *myList);
-void insert(NODE **root, char *data, int line);
+void insert(NODE **root, char *data, int line, int *grThree);
 LIST *addToList();
-void destructTree(NODE *root);
+void destructTree(NODE *root, int *uniqueWords);
 void printList(LIST *myList, FILE *fp);
 void printTreeInorder(NODE *root);
 NODE *allocateNode(char *data, int line);
-void writeArray(char program[][80], FILE *fp, int lineNum);
+void writeArray(char program[][300], FILE *fp, int lineNum);
 
 #endif
